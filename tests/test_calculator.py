b27b1fe9f7477a1490ba96a64bb077fe3f7e07d3
Enrico Nunes <enriconunes02@hotmail.com>
2026-04-20 15:23:21 +0100

    first commit

---
+++ tests/test_calculator.py
@@ -0,0 +1,13 @@
+from functions.calculator import add, multiply, divide
+
+
+def test_add():
+    assert add(3, 2) == 5
+
+
+def test_multiply():
+    assert multiply(3, 4) == 12
+
+
+def test_divide():
+    assert divide(10, 2) == 5.0
